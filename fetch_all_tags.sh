4bb4c2111483195879e4fa7deee4bc9132581c0e
Kuntal Majumder <kuntal@infracloud.io>
2021-01-07 21:55:25 +0530

    updated workflows

--- fetch_all_tags.sh
+++ fetch_all_tags.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-UPSTREAM_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/istio/istio.git"
+UPSTREAM_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/beepitybop/newrep.git"
+git tag -l | grep -E "^[0-9]+.[0-9]+.[0-9]+$" > f1
 git fetch --tags upstream
+git tag -l | grep -E "^[0-9]+.[0-9]+.[0-9]+$" > f2
+
+tags=$(comm -13 f1 f2)
+rm f1 f2
+
+for tag in $tags; do
+    git checkout tetrate-release-$tag
+    git merge $tag
+    git tag tetrate-test-$tag
+    git push orgin tetrate-release-$tag --tags
+done
+
 git push --tags origin
